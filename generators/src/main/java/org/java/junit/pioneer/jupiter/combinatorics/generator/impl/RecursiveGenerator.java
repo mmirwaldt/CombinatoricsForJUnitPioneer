76828440f76743f04f89274dd118176b3ee4c709
mmirwaldt <michael.mirwaldt@gmail.com>
2023-10-31 22:41:14 +0100

    refactored RecursiveGenerator by extracting RecursiveCombiner

--- generators/src/main/java/org/java/junit/pioneer/jupiter/combinatorics/generator/impl/RecursiveGenerator.java
+++ generators/src/main/java/org/java/junit/pioneer/jupiter/combinatorics/generator/impl/RecursiveGenerator.java
@@ -34,44 +34,14 @@ public class RecursiveGenerator implements CombinatoricsGenerator {
     @Override
     public <E> Set<Set<E>> combineWithoutRepetition(Set<E> elements, int length) {
         Set<Set<E>> result = new HashSet<>();
-        combineWithoutRepetition(List.copyOf(elements), length, new ArrayDeque<>(), result);
+        RecursiveCombiner.combineWithoutRepetition(List.copyOf(elements), length, new ArrayDeque<>(), result);
         return result;
     }
 
     @Override
     public <E> Set<Map<E, Integer>> combineWithRepetition(Set<E> elements, int length) {
         Set<Map<E, Integer>> result = new HashSet<>();
-        combineWithRepetition(List.copyOf(elements), length, new HashMap<>(), 0, result);
+        RecursiveCombiner.combineWithRepetition(List.copyOf(elements), length, new HashMap<>(), 0, result);
         return result;
-    }
-
-    public <E> void combineWithRepetition(List<E> elements, int k, Map<E, Integer> combination, int size, Set<Map<E, Integer>> results) {
-        if (size == k) {
-            results.add(new HashMap<>(combination));
-        } else {
-            int start = 0;
-            for (E element : elements) {
-                List<E> remainingElements = elements.subList(start, elements.size());
-                combination.compute(element, (key, value) -> (value == null) ? 1 : value + 1);
-                combineWithRepetition(remainingElements, k, combination, size + 1, results);
-                combination.compute(element, (key, value) -> (value == null || value == 1) ? null : value - 1);
-                start++;
-            }
-        }
-    }
-
-    public <E> void combineWithoutRepetition(List<E> elements, int k, Deque<E> variation, Set<Set<E>> results) {
-        if (variation.size() == k) {
-            results.add(new HashSet<>(variation));
-        } else {
-            int start = 0;
-            for (E element : elements) {
-                List<E> remainingElements = elements.subList(start + 1, elements.size());
-                variation.push(element);
-                combineWithoutRepetition(remainingElements, k, variation, results);
-                variation.pop();
-                start++;
-            }
-        }
     }
 }
