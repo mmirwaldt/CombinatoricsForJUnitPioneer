dd3a05096abbc9b78577733ef5efa72e0251a94b
mmirwaldt <michael.mirwaldt@gmail.com>
2023-11-20 01:33:43 +0100

    finished VariationWithoutRepetitionNumber with its tests in VariationWithoutRepetitionNumberTest

--- numbers/src/main/java/org/java/junit/pioneer/jupiter/combinatorial/numbers/VariationWithoutRepetitionNumber.java
+++ numbers/src/main/java/org/java/junit/pioneer/jupiter/combinatorial/numbers/VariationWithoutRepetitionNumber.java
@@ -1,20 +1,113 @@
 package org.java.junit.pioneer.jupiter.combinatorial.numbers;
 
+import java.util.*;
+
+import static java.util.Collections.nCopies;
+
 public class VariationWithoutRepetitionNumber extends CombinatorialNumber {
+    private final List<Integer> sortedUnusedDigitsForPlace;
+
+    /**
+     * stack for indices of sortedUnusedDigits.
+     */
+    private final Deque<Integer> indicesStack;
+
     public VariationWithoutRepetitionNumber(int base, int length) {
         super(base, length);
+        checkParameters(base, length);
+        initDigits(length);
+        sortedUnusedDigitsForPlace = new ArrayList<>(base);
+        indicesStack = new ArrayDeque<>(nCopies(length, 0));
+        initUnusedDigitsForLastPlace(base, length);
+    }
+
+    private void initDigits(int length) {
         for (int i = 0; i < length; i++) {
             digits[i] = (byte) i;
         }
     }
 
+    private void initUnusedDigitsForLastPlace(int base, int length) {
+        for (int i = length; i < base; i++) {
+            sortedUnusedDigitsForPlace.add(i);
+        }
+    }
+
     @Override
     public void increment() {
-        int increment = digits[0] + 1;
-        if(increment < base) {
-            digits[0]++;
-        } else {
-            throw new ArithmeticException("Overflow of number with base " + base + "!");
+        nonCarryOver();
+        carryOver();
+    }
+
+    private void nonCarryOver() {
+        int nextNonCarryOverIndex = lastNonCarryOverIndex();
+        reuseLastDigit();
+        useNextDigitAt(nextNonCarryOverIndex);
+    }
+
+    private void carryOver() {
+        for (int i = indicesStack.size(); i < digits.length; i++) {
+            useNextDigitAt(0);
+        }
+    }
+
+    private void useNextDigitAt(int nextIndex) {
+        int unusedDigit = sortedUnusedDigitsForPlace.get(nextIndex);
+        useDigit(unusedDigit);
+        iterateForward(nextIndex);
+    }
+
+    private void iterateForward(int nextIndex) {
+        indicesStack.push(nextIndex);
+    }
+
+    private void useDigit(int unusedDigit) {
+        digits[indicesStack.size()] = (byte) unusedDigit;
+        sortedUnusedDigitsForPlace.remove((Integer) unusedDigit);
+    }
+
+    private int lastNonCarryOverIndex() {
+        int index = nextIndex();
+        while (!hasUnusedDigitFor(index)) {
+            if (canCarryOver()) {
+                reuseLastDigit();
+                index = nextIndex();
+            } else {
+                throw new ArithmeticException("Overflow of number with base " + base + "!");
+            }
+        }
+        return index;
+    }
+
+    private boolean canCarryOver() {
+        return !indicesStack.isEmpty();
+    }
+
+    private boolean hasUnusedDigitFor(int index) {
+        return index <= sortedUnusedDigitsForPlace.size();
+    }
+
+    private int nextIndex() {
+        return indicesStack.pop() + 1;
+    }
+
+    private void reuseLastDigit() {
+        int oldDigit = digits[indicesStack.size()];
+        int insertIndex = Collections.binarySearch(sortedUnusedDigitsForPlace, oldDigit);
+        sortedUnusedDigitsForPlace.add((-insertIndex) - 1, oldDigit);
+    }
+
+    private static void checkParameters(int base, int length) {
+        if (base < 2 || Byte.MAX_VALUE < base) {
+            throw new IllegalArgumentException("The number of elements must lie in [1, " + Byte.MAX_VALUE + "]" +
+                    " and may not be " + length + "!");
+        }
+        if (length < 1) {
+            throw new IllegalArgumentException("The length must be positive, i.e. it must larger than 0, and not be " + length);
+        }
+        if (base < length) {
+            throw new IllegalArgumentException("No variations without repetition possible for the length " + length
+                    + " with " + base + " elements. The length must be smaller or equal than the number of elements");
         }
     }
 }
--- numbers/src/test/java/org/java/junit/pioneer/jupiter/combinatorial/numbers/VariationWithoutRepetitionNumberTest.java
+++ numbers/src/test/java/org/java/junit/pioneer/jupiter/combinatorial/numbers/VariationWithoutRepetitionNumberTest.java
@@ -47,51 +47,81 @@ public class VariationWithoutRepetitionNumberTest {
         assertThrows(ArithmeticException.class, number::increment);
     }
 
-//    @Test
-//    void test_2_binary_digits_increment_once() {
-//        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(2, 2);
-//        assertEquals(0, number.digit(0));
-//        assertEquals(1, number.digit(1));
-//        number.increment();
-//        assertEquals(1, number.digit(0));
-//        assertEquals(0, number.digit(1));
-//    }
-//
-//    @Test
-//    void test_2_binary_digits_increment_twice_to_overflow() {
-//        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(2, 2);
-//        number.increment();
-//        assertThrows(ArithmeticException.class, number::increment);
-//    }
-//
-//    @Test
-//    void test_3_trinary_digits_increment_012_to_210() {
-//        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(3, 3);
-//        assertEquals(0, number.digit(0));
-//        assertEquals(1, number.digit(1));
-//        assertEquals(2, number.digit(2));
-//        number.increment();
-//        assertEquals(0, number.digit(0));
-//        assertEquals(2, number.digit(1));
-//        assertEquals(1, number.digit(2));
-//        number.increment();
-//        assertEquals(1, number.digit(0));
-//        assertEquals(0, number.digit(1));
-//        assertEquals(2, number.digit(2));
-//        number.increment();
-//        assertEquals(1, number.digit(0));
-//        assertEquals(2, number.digit(1));
-//        assertEquals(0, number.digit(2));
-//        number.increment();
-//        assertEquals(2, number.digit(0));
-//        assertEquals(0, number.digit(1));
-//        assertEquals(1, number.digit(2));
-//        number.increment();
-//        assertEquals(2, number.digit(0));
-//        assertEquals(1, number.digit(1));
-//        assertEquals(0, number.digit(2));
-//    }
-//
+    @Test
+    void test_2_trinary_digits() {
+        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(3, 2);
+        assertEquals(0, number.digit(0));
+        assertEquals(1, number.digit(1));
+    }
+
+    @Test
+    void test_2_trinary_digits_increment_carry_over() {
+        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(3, 2);
+        assertEquals(0, number.digit(0));
+        assertEquals(1, number.digit(1));
+        number.increment();
+        assertEquals(0, number.digit(0));
+        assertEquals(2, number.digit(1));
+        number.increment();
+        assertEquals(1, number.digit(0));
+        assertEquals(0, number.digit(1));
+    }
+
+    @Test
+    void test_2_trinary_digits_increment_carry_over_and_increment() {
+        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(3, 2);
+        number.increment();
+        number.increment();
+        number.increment();
+        assertEquals(1, number.digit(0));
+        assertEquals(2, number.digit(1));
+    }
+
+    @Test
+    void test_2_binary_digits_increment_once() {
+        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(2, 2);
+        assertEquals(0, number.digit(0));
+        assertEquals(1, number.digit(1));
+        number.increment();
+        assertEquals(1, number.digit(0));
+        assertEquals(0, number.digit(1));
+    }
+
+    @Test
+    void test_2_binary_digits_increment_twice_to_overflow() {
+        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(2, 2);
+        number.increment();
+        assertThrows(ArithmeticException.class, number::increment);
+    }
+
+    @Test
+    void test_3_trinary_digits_increment_012_to_210() {
+        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(3, 3);
+        assertEquals(0, number.digit(0));
+        assertEquals(1, number.digit(1));
+        assertEquals(2, number.digit(2));
+        number.increment();
+        assertEquals(0, number.digit(0));
+        assertEquals(2, number.digit(1));
+        assertEquals(1, number.digit(2));
+        number.increment();
+        assertEquals(1, number.digit(0));
+        assertEquals(0, number.digit(1));
+        assertEquals(2, number.digit(2));
+        number.increment();
+        assertEquals(1, number.digit(0));
+        assertEquals(2, number.digit(1));
+        assertEquals(0, number.digit(2));
+        number.increment();
+        assertEquals(2, number.digit(0));
+        assertEquals(0, number.digit(1));
+        assertEquals(1, number.digit(2));
+        number.increment();
+        assertEquals(2, number.digit(0));
+        assertEquals(1, number.digit(1));
+        assertEquals(0, number.digit(2));
+    }
+
 //    @Test
 //    void test_3_trinary_digits_increment_to_overflow() {
 //        VariationWithoutRepetitionNumber number = new VariationWithoutRepetitionNumber(3, 3);
